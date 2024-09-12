4ac00c3930f196467d5b7a79d9988795878967d4
mjgonzalez59 <mjulissagonzalez@gmail.com>
2024-09-12 16:22:34 -0500

    Instaling Icons

--- src/app/modules/about-us/component/about-us/about-us.component.ts
+++ src/app/modules/about-us/component/about-us/about-us.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-about-us',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './about-us.component.html',
   styleUrl: './about-us.component.css'
 })
 export class AboutUsComponent {
-
+  faCoffee = faCoffee;
 }
