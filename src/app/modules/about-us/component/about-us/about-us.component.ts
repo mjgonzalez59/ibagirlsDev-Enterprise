aacbfb0a7bcd2123d0bb95e64ed1ceff53b702e0
mjgonzalez59 <mjulissagonzalez@gmail.com>
2024-09-12 19:03:22 -0500

    Installing angular material and colors in Tailwind

--- src/app/modules/about-us/component/about-us/about-us.component.ts
+++ src/app/modules/about-us/component/about-us/about-us.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+
 
 @Component({
   selector: 'app-about-us',
   standalone: true,
-  imports: [FontAwesomeModule],
+  imports: [FontAwesomeModule, MatSlideToggleModule],
   templateUrl: './about-us.component.html',
   styleUrl: './about-us.component.css'
 })
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}", "./node_modules/flowbite/**/*.js"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "color-black": "#222140",
+        "color-yellow": "#EFF29B",
+        "color-lila": "#B091F2",
+        "color-fucsia": "#CA6BF2",
+        "color-pink": "#F2798F",
+        "color-white": "#FFFFFF",
+        "color-blue": "#95B2E9",
+      },
+    },
   },
   plugins: [require("flowbite/plugin")],
 };
